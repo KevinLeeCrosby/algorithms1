15999499a561cede9324cc02c09a5a271e44fd08
Kevin L. Crosby <Kevin.L.Crosby@gmail.com>
2015-02-21 00:07:04 -0600

    8puzzle submission #5.

--- src/main/java/4/Board.java
+++ src/main/java/4/Board.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * @author Kevin Crosby
  */
 public class Board {
-  private static final Map<char[], Integer> manhattan = new HashMap<>();
+  private static Map<char[], Integer> manhattan = new HashMap<>();
   private final int N;
   private final int N2;
   private final char[] grid;
